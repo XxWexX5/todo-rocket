a72b15b650ce20ae9f11b4e0add81de0152eb491
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-24 18:01:28 -0300

    [ FEATURE ] Adding font Inter in the project.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,28 @@
 import "../styles/global.css";
 
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
+
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter: require("../assets/fonts/Inter/inter.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <>
       <StatusBar
@@ -13,7 +33,7 @@ export default function RootLayout() {
 
       <SafeAreaView className="mt-[7vh] mx-[5vw]">
         <View>
-          <Text className="text-blue-500">Hello World!</Text>
+          <Text className="text-blue-500 font-inter">Hello World!</Text>
         </View>
       </SafeAreaView>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
